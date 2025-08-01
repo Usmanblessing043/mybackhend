fe94b3f9662fea941603f4c45fcf8a305dbac295
Usmanblessing043 <usmanblesssing043@gmail.com>
2025-07-31 13:17:15 -1100

    backhen

--- middleware/user.validation.js
+++ middleware/user.validation.js
@@ -2,7 +2,7 @@ const yup = require ("yup")
 
 
 const uservalidation = yup.object({
-    username:yup.string().trim().matches(/ ^[a-z0-9_-]{3,15}$/,"username must include the following uppercase lowercase").required("username is required"),
+    username:yup.string().trim().required("username is required"),
     email:yup.string().email("input a valid email").required("email is required"),
     password:yup.string().trim().required("password is required")
     
